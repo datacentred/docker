d7ea1b77e8bf48321d76b07a5de6fd035e10e515
Nick Jones <nick.jones@datacentred.co.uk>
2016-08-31 17:08:45 +0100

    [Horizon] stdout and stderr log redirection
    This commit configures logging for Apache httpd and Django so that logs
    are sent to stdout and stderr.  It also configures supervisord to
    redirect the output from std{out,err} of these child processes to its
    own stdout which is then visible via the `docker logs` command.

--- puppet/modules/profile/manifests/openstack/horizon.pp
+++ puppet/modules/profile/manifests/openstack/horizon.pp
@@ -5,4 +5,20 @@
 class profile::openstack::horizon {
   include ::horizon
   include ::branding::horizon
+
+  file { '/var/log/apache2/horizon_access.log':
+    target  => '/dev/stdout',
+    require => Package['httpd'],
+  }
+
+  file { '/var/log/apache2/horizon_error.log':
+    target  => '/dev/stderr',
+    require => Package['httpd'],
+  }
+
+  file { '/var/log/apache2/error.log':
+    target  => '/dev/stderr',
+    require => Package['httpd'],
+  }
+
 }
