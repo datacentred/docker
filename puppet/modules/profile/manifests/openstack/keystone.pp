7dff3087fd621b1177f0e767fd269aed19c4aa99
Nick Jones <nick.jones@datacentred.co.uk>
2016-10-08 21:30:18 +0100

    [Keystone] Log to stdout / stderr

--- puppet/modules/profile/manifests/openstack/keystone.pp
+++ puppet/modules/profile/manifests/openstack/keystone.pp
@@ -5,4 +5,29 @@ class profile::openstack::keystone {
   include ::keystone
   include ::keystone::wsgi::apache
 
+  file { '/var/log/apache2/keystone_wsgi_main_access.log':
+    target  => '/dev/stdout',
+    require => Package['httpd'],
+  }
+
+  file { '/var/log/apache2/keystone_wsgi_main_error.log':
+    target  => '/dev/stderr',
+    require => Package['httpd'],
+  }
+
+  file { '/var/log/apache2/keystone_wsgi_admin_access.log':
+    target  => '/dev/stdout',
+    require => Package['httpd'],
+  }
+
+  file { '/var/log/apache2/keystone_wsgi_admin_error.log':
+    target  => '/dev/stderr',
+    require => Package['httpd'],
+  }
+
+  file { '/var/log/apache2/error.log':
+    target  => '/dev/stderr',
+    require => Package['httpd'],
+  }
+
 }
