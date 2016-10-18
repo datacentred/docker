b231c5ad2522914338e10fb7bb4ae1bef4e86e96
Nick Jones <nick.jones@datacentred.co.uk>
2016-10-18 15:01:38 +0100

    [Glance] Switch to using Keystone authtoken classes
    Use the new ::glance::{api,registry}::authtoken classes which maintain
    forwards compatibility in-line with the switch to Keystone V3.

--- puppet/modules/profile/manifests/openstack/glance.pp
+++ puppet/modules/profile/manifests/openstack/glance.pp
@@ -2,7 +2,9 @@
 #
 class profile::openstack::glance {
   include ::glance::api
+  include ::glance::api::authtoken
   include ::glance::registry
+  include ::glance::registry::authtoken
   include ::glance::notify::rabbitmq
   include ::glance::policy
   include ::glance::backend::rbd
