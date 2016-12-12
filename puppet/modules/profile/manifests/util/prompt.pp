3038390ebbd9b26ff9e234a26fc44530062738b5
Nick Jones <nick.jones@datacentred.co.uk>
2016-12-12 14:00:31 +0000

    [Nova] Configuration for Nova components in a Docker container (#14)
    This adds the configuration detail necessary to build the Nova
    components in a Docker container.
    
    It also updates the version of Puppet used in the process to 1.8.2, adds
    a new 'domain' element to the Hiera hierarchy.

---
+++ puppet/modules/profile/manifests/util/prompt.pp
@@ -0,0 +1,13 @@
+# == Class: ::profile::util::prompt
+#
+# Prefix the shell prompt with the name of the container
+#
+class profile::util::prompt {
+  file { '/etc/debian_chroot':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => $::role,
+  }
+}
