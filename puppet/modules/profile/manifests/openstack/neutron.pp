fd22a1a99ada06fb0d907c42ed4f974b7a86e337
Nick Jones <nick.jones@datacentred.co.uk>
2017-03-29 16:51:00 +0100

    [Neutron] Server configuration for Liberty release (#37)
    Appropriately configure Neutron's authentication and Nova notification
    settings for the Liberty release.

--- puppet/modules/profile/manifests/openstack/neutron.pp
+++ puppet/modules/profile/manifests/openstack/neutron.pp
@@ -4,8 +4,24 @@
 class profile::openstack::neutron {
   include ::neutron
   include ::neutron::server
-  include ::neutron::keystone::authtoken
   include ::neutron::server::notifications
   include ::neutron::plugins::ml2
   include ::neutron::quota
+
+  ensure_packages(['python-neutron-lbaas'])
+
+  # TODO: Remove these hacky workarounds once we're at a version of OpenStack that's
+  # properly supported by puppet-nova
+  neutron_config {
+    'keystone_authtoken/auth_plugin':         value => 'password';
+    'keystone_authtoken/auth_url':            value => "https://${hiera('os_api_host')}:35357";
+    'keystone_authtoken/username':            value => 'neutron';
+    'keystone_authtoken/password':            value => hiera('keystone_neutron_password');
+    'keystone_authtoken/project_domain_name': value => 'default';
+    'keystone_authtoken/user_domain_name':    value => 'default';
+    'keystone_authtoken/project_name':        value => 'services';
+    'nova/project_domain_name':               value => 'default';
+    'nova/user_domain_name':                  value => 'default';
+  }
+
 }
