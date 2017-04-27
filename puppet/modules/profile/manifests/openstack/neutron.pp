f893ea9c3ae5813c2f69f75711258fa530ce5197
Nick Jones <nick.jones@datacentred.co.uk>
2017-04-27 16:11:01 +0100

    [Neutron] Re-align container configuration with Production (#50)
    Whilst updating the Neutron Server profile class I spotted a couple of
    other recently-added options that weren't handled in the Docker
    equivalent.
    
    This commit should align the two and ensure consistency.

--- puppet/modules/profile/manifests/openstack/neutron.pp
+++ puppet/modules/profile/manifests/openstack/neutron.pp
@@ -13,15 +13,16 @@ class profile::openstack::neutron {
   # TODO: Remove these hacky workarounds once we're at a version of OpenStack that's
   # properly supported by puppet-nova
   neutron_config {
-    'keystone_authtoken/auth_plugin':         value => 'password';
-    'keystone_authtoken/auth_url':            value => "https://${hiera('os_api_host')}:35357";
-    'keystone_authtoken/username':            value => 'neutron';
-    'keystone_authtoken/password':            value => hiera('keystone_neutron_password');
-    'keystone_authtoken/project_domain_name': value => 'default';
-    'keystone_authtoken/user_domain_name':    value => 'default';
-    'keystone_authtoken/project_name':        value => 'services';
-    'nova/project_domain_name':               value => 'default';
-    'nova/user_domain_name':                  value => 'default';
+    'keystone_authtoken/auth_plugin':                          value => 'password';
+    'keystone_authtoken/auth_url':                             value => "https://${hiera('os_api_host')}:35357";
+    'keystone_authtoken/username':                             value => 'neutron';
+    'keystone_authtoken/password':                             value => hiera('keystone_neutron_password');
+    'keystone_authtoken/project_domain_name':                  value => 'default';
+    'keystone_authtoken/user_domain_name':                     value => 'default';
+    'keystone_authtoken/project_name':                         value => 'services';
+    'nova/project_domain_name':                                value => 'default';
+    'nova/user_domain_name':                                   value => 'default';
+    'DEFAULT/enable_services_on_agents_with_admin_state_down': value => true;
   }
 
 }
