f16d9ee2e21a9201839279516e13a60d24fc516e
Nick Jones <nick@dischord.org>
2017-07-24 09:45:39 +0100

    [Keystone] Upgrade to Newton
    This commit switches the default repo for Keystone to Newton and
    installs updated packages accordingly.  It also amends some of the
    Puppet configuration inline with changes to the `puppet-keystone`
    module.
    
    🔧 PD-3090

--- modules/profile/manifests/openstack/keystone.pp
+++ modules/profile/manifests/openstack/keystone.pp
@@ -6,12 +6,6 @@ class profile::openstack::keystone {
   include ::keystone
   include ::keystone::wsgi::apache
 
-  file_line { 'keystone_nofiles':
-    path    => '/etc/init/keystone.conf',
-    line    => 'limit nofile 4096 65536',
-    require => Package['keystone'],
-  }
-
   file { '/var/log/apache2/keystone_wsgi_main_access.log':
     target  => '/dev/stdout',
     require => Package['httpd'],
